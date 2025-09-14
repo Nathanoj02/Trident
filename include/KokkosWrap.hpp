35e17d309336946103bdb7e6afccd6db1edc437d
LorenzoPichetti <lori.pichi@gmail.com>
2025-09-14 11:51:21 -0400

    KokkosWrap on a separate file

---
+++ include/KokkosWrap.hpp
@@ -0,0 +1,33 @@
+#ifndef KOKKOS_WRAP_H
+#define KOKKOS_WRAP_H
+
+#include <dmmio/dmmio.h>
+
+#include <Kokkos_Core.hpp>
+#include <KokkosSparse_CrsMatrix.hpp>
+#include <KokkosSparse_CcsMatrix.hpp>
+
+namespace KokkosWrap {
+    enum class MajorDim {
+        ROWS, // i.e. CSR
+        COLS  // i.e. CSC
+    };
+
+    template <typename KIT, typename DIT, typename VT>
+    struct Matrix {
+        dmmio::Partitioning* partitioning;  // still raw pointer to external partitioning
+
+        // Instead of a raw union, use std::variant for safety
+        std::variant<
+            KokkosSparse::CrsMatrix<VT, KIT, Kokkos::DefaultExecutionSpace, void, KIT>,
+            KokkosSparse::CcsMatrix<VT, KIT, Kokkos::DefaultExecutionSpace, void, KIT>
+        > storage;
+
+        MajorDim layout;  // which one we actually hold
+
+        // ---- Constructor ----
+        Matrix(dmmio::DCOO<DIT, VT>* dcoo, MajorDim T);
+    };
+}
+
+#endif // KOKKOS_WRAP_H
--- test/test_kokkos.cu
+++ test/test_kokkos.cu
@@ -20,71 +20,10 @@
 
 #include "../include/test_utils.cuh"
 
-#include <Kokkos_Core.hpp>
-#include <KokkosSparse_CrsMatrix.hpp>
-#include <KokkosSparse_CooMatrix.hpp>
-#include <KokkosSparse_CcsMatrix.hpp>
-#include <KokkosSparse_coo2crs.hpp>
-#include <KokkosSparse_crs2ccs.hpp>
-#include <KokkosSparse_ccs2crs.hpp>
+#include <KokkosWrap.hpp>
 
 #define OPSTR(X) ((X == dmmio::Operation::None) ? ("None") : ("Transpose") )
 
-namespace KokkosWrap {
-    enum class MajorDim {
-        ROWS, // i.e. CSR
-        COLS  // i.e. CSC
-    };
-
-    template <typename KIT, typename DIT, typename VT>
-    struct Matrix {
-        dmmio::Partitioning* partitioning;  // still raw pointer to external partitioning
-
-        // Instead of a raw union, use std::variant for safety
-        std::variant<
-            KokkosSparse::CrsMatrix<VT, KIT, Kokkos::DefaultExecutionSpace, void, KIT>,
-            KokkosSparse::CcsMatrix<VT, KIT, Kokkos::DefaultExecutionSpace, void, KIT>
-        > storage;
-
-        MajorDim layout;  // which one we actually hold
-
-        // ---- Constructor ----
-        Matrix(dmmio::DCOO<DIT, VT>* dcoo, MajorDim T)
-            : partitioning(dcoo->partitioning), layout(T)
-        {
-            int nnz = dcoo->coo->nnz;
-            Kokkos::View<KIT*> row_d("row", nnz);
-            Kokkos::View<KIT*> col_d("col", nnz);
-            Kokkos::View<VT*> val_d("val", nnz);
-
-            auto row_h = Kokkos::create_mirror_view(row_d);
-            auto col_h = Kokkos::create_mirror_view(col_d);
-            auto val_h = Kokkos::create_mirror_view(val_d);
-
-            for (int i = 0; i < nnz; i++) {
-                row_h(i) = static_cast<KIT>(dcoo->coo->row[i]);
-                col_h(i) = static_cast<KIT>(dcoo->coo->col[i]);
-                val_h(i) = static_cast<VT>(1.0); // TODO: use real values if available
-            }
-
-            Kokkos::deep_copy(row_d, row_h);
-            Kokkos::deep_copy(col_d, col_h);
-            Kokkos::deep_copy(val_d, val_h);
-
-            // --- Step 1: COO → CSR (row-major) ---
-            auto csr = KokkosSparse::coo2crs(dcoo->coo->nrows, dcoo->coo->ncols, row_d, col_d, val_d);
-
-            // --- Step 2: Decide layout ---
-            if (T == MajorDim::ROWS) {
-                storage = csr; // keep CSR
-            } else {
-                // CSR → CSC (column-major)
-                auto csc = KokkosSparse::crs2ccs(csr);
-                storage = csc; // store CSC
-            }
-        }
-    };
-}
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -171,6 +110,9 @@ int main(int argc, char** argv) {
     dmmio::ProcessGrid *Cgrid = dcoo_A->partitioning->grid; // NOTE BUG TODO: we nead to think something for the grid managing.
     dmmio::utils::ProcessGrid_graph(dcoo_A->partitioning->grid, stdout);
     MPI_Barrier(MPI_COMM_WORLD);
+
+    dmmio::partitioning::indextransform::transformCoo::global2group(dcoo_A);
+    dmmio::partitioning::indextransform::transformCoo::global2group(dcoo_B);
 
     Kokkos::initialize(argc, argv);
     {
