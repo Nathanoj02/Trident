7bfca0960e2cc4f34c570e1d9d26178df1b1251a
Thomas Pasquali <thomas.pasquali@unitn.it>
2025-09-24 10:12:08 +0200

    Preliminary evaluation (draft)

---
+++ scripts/trilinos_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+set -e
+
+source scripts/variables.sh
+
+[[ -d Trilinos ]] || git clone https://github.com/trilinos/Trilinos.git
+
+cmake -B Trilinos/build \
+  -D CMAKE_BUILD_TYPE=RELEASE \
+  -D CMAKE_INSTALL_PREFIX="${Trilinos_PREFIX}" \
+  -D CMAKE_CUDA_COMPILER="$(which nvcc)" \
+  -D Kokkos_ENABLE_CUDA_LAMBDA=ON \
+  -D Kokkos_ARCH_AMPERE80=ON \
+  -D Trilinos_SET_GROUP_AND_PERMISSIONS_ON_INSTALL_BASE_DIR="${Trilinos_PREFIX}" \
+  -D Trilinos_ENABLE_INSTALL_CMAKE_CONFIG_FILES=ON \
+  -D Trilinos_ENABLE_Kokkos=ON \
+  -D Trilinos_ENABLE_Fortran=OFF \
+  -D Trilinos_ENABLE_TESTS=OFF \
+  -D Trilinos_ENABLE_Tpetra=ON \
+  -D Trilinos_ENABLE_Teuchos=ON \
+  -D TPL_ENABLE_MPI=ON \
+  -D TPL_ENABLE_CUDA=ON \
+  -D KOKKOSKERNELS_ENABLE_TPL_CUBLAS=OFF \
+  -D KOKKOSKERNELS_ENABLE_TPL_CUSOLVER=OFF \
+  -D KOKKOSKERNELS_ENABLE_TPL_CUSPARSE=OFF \
+  Trilinos
