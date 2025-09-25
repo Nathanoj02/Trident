364052df025c89e4f36c5afec8587eeea76f5911
Hooninator <jbellavita@berkeley.edu>
2025-09-25 15:43:20 -0700

    trilinos build update

--- scripts/trilinos_spgemm_build.sh
+++ scripts/trilinos_spgemm_build.sh
@@ -4,9 +4,11 @@ set -e
 
 source scripts/variables.sh
 
+cd comparison
+
 cmake -S . -B build_trilinos \
     -DCMAKE_CXX_COMPILER="$CMAKE_CXX_COMPILER" \
     -DENABLE_TRILINOS=ON
 
 cd build_trilinos
-make -j16 
+make VERBOSE=1 trilinos_spgemm -j16 
